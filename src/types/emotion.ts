fa2543e96add377dbb1d029185f6cdfb75990ec0
QwehhD <adpmahardika09@gmail.com>
2026-06-16 19:33:03 +0700

    feat: ownername?

--- src/types/emotion.ts
+++ src/types/emotion.ts
@@ -4,6 +4,7 @@ export interface EmotionLog {
   id: string;
   name: string;
   card_uid: string;
+  owner_name?: string; // ← tambah ini (optional)
   emotion: EmotionType;
   timestamp: string;
 }
